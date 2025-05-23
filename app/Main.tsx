5d7ba5371506c23699d1b9f08c979c3bb8693827
Zion Emond <zionem7@gmail.com>
2025-05-23 12:56:50 -0400

    began customizing homepage

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,19 +1,38 @@
 import Link from '@/components/Link';
 import Tag from '@/components/Tag';
 import siteMetadata from '@/data/siteMetadata';
+import Image from 'next/image';
 import { formatDate } from 'pliny/utils/formatDate';
-import NewsletterForm from 'pliny/ui/NewsletterForm';
 
 const MAX_DISPLAY = 5;
 
 export default function Home({ posts }) {
     return (
         <>
+            <div className="container mb-2 flex h-[80vh] flex-col items-center justify-center">
+                <h1 className="text-textHeading-light dark:text-textHeading text-3xl">
+                    Zion Emond
+                </h1>
+                <p className="text-textBody-light dark:text-textBody mb-2">lorem ipsum sit dolor</p>
+                <a
+                    className="bg-koi hover:bg-koiHover dark:bg-matcha dark:hover:bg-matchaHover m-2 max-w-1/2 rounded-full p-2"
+                    href="/portfolio"
+                >
+                    My projects
+                </a>
+                <Image
+                    src="/static/images/logo.png"
+                    alt="koi fish logo"
+                    className="absolute right-100 bottom-25 h-auto w-64 opacity-20"
+                    height={200}
+                    width={200}
+                />
+            </div>
             <div className="divide-y divide-gray-200 dark:divide-gray-700">
                 <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-                    <h1 className="text-textHeading-light-light dark:text-textHeading text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                    <h2 className="text-textHeading-light-light dark:text-textHeading text-2xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                         Latest
-                    </h1>
+                    </h2>
                     <p className="dark:text-textBody text-lg leading-7 text-gray-500">
                         {siteMetadata.description}
                     </p>
@@ -37,14 +56,14 @@ export default function Home({ posts }) {
                                         <div className="space-y-5 xl:col-span-3">
                                             <div className="space-y-6">
                                                 <div>
-                                                    <h2 className="text-2xl leading-8 font-bold tracking-tight">
+                                                    <h3 className="text-2xl leading-8 font-bold tracking-tight">
                                                         <Link
                                                             href={`/blog/${slug}`}
                                                             className="text-textHeading-light dark:text-textHeading"
                                                         >
                                                             {title}
                                                         </Link>
-                                                    </h2>
+                                                    </h3>
                                                     <div className="flex flex-wrap">
                                                         {tags.map((tag) => (
                                                             <Tag key={tag} text={tag} />
