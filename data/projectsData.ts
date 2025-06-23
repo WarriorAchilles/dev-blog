823d117e51a16b507ee2ca5667cb3048358557a2
Zion Emond <zionem7@gmail.com>
2025-06-23 08:26:30 -0400

    added nanohabits to portfolio

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,20 +10,26 @@ const projectsData: Project[] = [
         title: 'CQL',
         description: `My current day job. CQL is a custom ecommerce development agency that specializes in building high-quality, scalable, and secure ecommerce solutions. In my day to day role, I work with a team of talented developers with technologies such as Reach, Node.js, TypeScript, ASP.NET, and more. I use platforms such as AWS, Azure, Shopify Plus, BigCommerce, and Salesforce Commerce Cloud, to build solutions that are the best fit for our clients.`,
         imgSrc: '/static/images/CQL-Logo.png',
-        href: '/blog/CQL', // probably should make a blog post about this and then link the blog post here, then link to the site inside the blog post.
+        href: '/blog/CQL',
     },
     {
         title: 'MERC',
         description: `A website for Medical Equipment Resource Consultants, a company that specializes in helping hospitals and healthcare facilities maximize the value of their medical equipment. Made with WordPress.`,
         imgSrc: '/static/images/MERC-Homepage.png',
-        href: '/blog/merc', // probably should make a blog post about this and then link the blog post here, then link to the site inside the blog post.
+        href: '/blog/merc',
     },
-    // {
-    //     title: 'NanoHabits',
-    //     description: `More info coming soon`,
-    //     imgSrc: '/static/images/time-machine.jpg',
-    //     href: '#',
-    // },
+    {
+        title: 'NanoHabits',
+        description: `More info coming soon`,
+        imgSrc: '/static/images/time-machine.jpg',
+        href: '/blog/nanohabits',
+    },
+    {
+        title: 'NanoHabits Prototype',
+        description: `Discord bot`,
+        imgSrc: '/static/images/time-machine.jpg',
+        href: '/blog/nanohabits-prototype',
+    },
 ];
 
 export default projectsData;
