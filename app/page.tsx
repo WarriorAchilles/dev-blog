bb127cf4ab298b00cd353c9793401b15bcb448be
Zion Emond <zionem7@gmail.com>
2025-07-29 10:47:41 -0400

    add homepage background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer';
 import { allBlogs } from 'contentlayer/generated';
 import Main from './Main';
+import Beams from '@/components/Beams';
 
 export default async function Page() {
     const sortedPosts = sortPosts(allBlogs);
     const posts = allCoreContent(sortedPosts);
-    return <Main posts={posts} />;
+    return (
+        <>
+            <div
+                style={{
+                    width: '100%',
+                    height: '80vh',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    zIndex: -999,
+                }}
+            >
+                <Beams
+                    beamHeight={30}
+                    beamWidth={0.5}
+                    beamNumber={26}
+                    speed={2}
+                    noiseIntensity={2}
+                    scale={0.2}
+                    rotation={228}
+                    lightColor="#90a955"
+                />
+            </div>
+            <Main posts={posts} />
+        </>
+    );
 }
