b0272b98f2d3864159710d138d70fb3a42ec780b
Zion Emond <zion.emond@cqlcorp.com>
2025-08-28 08:49:44 -0400

    add contact info to homepage

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,9 +1,13 @@
 import Homepage from '@/components/Homepage';
+import { coreContent } from 'pliny/utils/contentlayer';
+import { Authors, allAuthors } from 'contentlayer/generated';
 
 export default function Home({ posts }) {
+    const author = allAuthors.find((p) => p.slug === 'default') as Authors;
+    const mainContent = coreContent(author);
     return (
         <>
-            <Homepage posts={posts} />
+            <Homepage posts={posts} content={mainContent} />
         </>
     );
 }
